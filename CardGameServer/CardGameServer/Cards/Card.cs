e10ba2202155e769ab878f24f49972512305836a
Nicholas Fleck <TheBerkin@users.noreply.github.com>
2018-09-18 10:15:08 -0500

    Initial commit

---
+++ CardGameServer/CardGameServer/Cards/Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGameServer.Cards
+{
+    struct Card
+    {
+        public CardRank Rank { get; set; }
+        public CardSuit Suit { get; set; }
+
+        public Card(CardRank rank, CardSuit suit)
+        {
+            Rank = rank;
+            Suit = suit;
+        }
+
+        public byte GetCardCode() => (byte)(((byte)Rank << 4) | (byte)Suit);
+
+        public static Card FromCardCode(byte code) => new Card();
+    }
+}
---
+++ CardGameServer/CardGameServer/Cards/CardSuit.cs
@@ -0,0 +1,10 @@
+﻿namespace CardGameServer.Cards
+{
+    enum CardSuit : byte
+    {
+        Clubs = 1,
+        Diamonds = 2,
+        Hearts = 3,
+        Spades = 4
+    }
+}
