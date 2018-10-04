6218445b6c5a9ffb4f2a46475582a0fa37dc600d
Farstad <fx6134ud@winona.edu>
2018-10-04 13:29:32 -0500

    As a file

---
+++ 410ProgrammingProject1/410ProgrammingProject1/src/control/StateBasedRunner.java
@@ -0,0 +1,27 @@
+package control;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+import view.GameState;
+import view.MenuState;
+
+public class StateBasedRunner extends StateBasedGame{
+
+	public static final int MENU = 0;
+	public static final int GAME = 1;
+	public static final int RESULT = 2;
+	
+	public StateBasedRunner(String name) {
+		super(name);
+	}
+
+	@Override
+	public void initStatesList(GameContainer gc) throws SlickException {
+		//addState(new MenuState());
+		addState(new GameState());
+		//addState(new ResultState());
+	}
+
+}
---
+++ 410ProgrammingProject1/410ProgrammingProject1/src/view/ResultState.java
@@ -0,0 +1,31 @@
+package view;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class ResultState extends BasicGameState{
+
+	@Override
+	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException {
+		
+	}
+
+	@Override
+	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2) throws SlickException {
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, StateBasedGame arg1, int arg2) throws SlickException {
+		
+	}
+
+	@Override
+	public int getID() {
+		return control.StateBasedRunner.RESULT;
+	}
+
+}
